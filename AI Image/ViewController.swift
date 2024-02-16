8f25b5ea122bd736be6797d4f3545716cbc7f311
keremdemir <keremdemir1694@gmail.com>
2024-02-16 15:56:46 +0300

    Initial Commit

---
+++ AI Image/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AI Image
+//
+//  Created by Kerem Demir on 16.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
