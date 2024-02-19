7e2c90ac01e713549662e78a9b4b51136fe55a38
keremdemir <keremdemir1694@gmail.com>
2024-02-19 18:15:47 +0300

    Initial commit.

---
+++ AI Image/APIService.swift
@@ -0,0 +1,14 @@
+//
+//  APIService.swift
+//  AI Image
+//
+//  Created by Kerem Demir on 19.02.2024.
+//
+
+import Foundation
+
+class APIService {
+    let apiKey = "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"
+    
+    
+}
